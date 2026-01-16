bb9567659988bd5e60da69767e2f93c84bda5db0
Stuart Baumann <13663771+s-baumann@users.noreply.github.com>
2026-01-17 00:23:46 +1100

    Update test_vs_vwap.jl

--- test/test_vs_vwap.jl
+++ test/test_vs_vwap.jl
@@ -346,5 +346,8 @@ end
         @test exec_data.summary[:bps].vs_vwap_slippage[1] ≈ expected_bps atol=0.1
         @test exec_data.summary[:pct].vs_vwap_slippage[1] ≈ expected_bps / 100 atol=0.01
         @test exec_data.summary[:usd].vs_vwap_slippage[1] ≈ expected_bps / 10000 * 100 * 200 atol=0.1
+
+        print_slippage_summary(exec_data)
+        plot_execution_markout(exec_data; execution_name="test_units")
     end
 end
